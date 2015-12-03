95d68be1a59f8e9e67fea7c503257effb81d6534
Jonathan Foster <jfoster@tradestation.com>
2015-12-03 11:23:10 -0500

    Initial commit

---
+++ src/Nancy.Demo.ErrorHandlerInvalidCastException/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Nancy.Demo.ErrorHandling.Models
+{
+    using System;
+
+    public class User
+    {
+        public User() :
+            this(null, null)
+        {
+        }
+
+        public User(string firstName, string lastName)
+        {
+            throw new Exception();
+
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ src/Nancy.Demo.ErrorHandlerInvalidCastException/Modules/MainModule.cs
@@ -0,0 +1,18 @@
+﻿namespace Nancy.Demo.ErrorHandling.Modules
+{
+    using Models;
+    using Nancy;
+
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            this.Get["/"] = _ =>
+            {
+                return this.Negotiate
+                    .WithView("Index")
+                    .WithModel(new User("Wayne", "Skylar"));
+            };
+        }
+    }
+}
